dc3a1b59b8dd711e9e1363da0e48ab8d6827105d
Amulya Kurapati <116165442+amulyakurapat@users.noreply.github.com>
2025-02-13 09:52:24 +0530

    Time: 10 ms, Memory: 40.4 MB - LeetHub

---
+++ 8525-380-509-fibonacci-number/8525-380-509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int fib(int n) {
+        if(n==0 ){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else {
+           return fib(n-1) + fib(n-2);
+        }
+    }
+    public static void main(String[] args){
+        int n = 2;
+        System.out.println(fib(2));
+
+    }
+}
